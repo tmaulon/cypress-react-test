99e5484303994c34cc177447a22eb4045b09e1e1
tmaulon <thomas.maulon@gmail.com>
2020-10-04 12:10:21 +0200

    improve tests to get application screenshots for each viewport sizes

--- cypress/integration/form.spec.ts
+++ cypress/integration/form.spec.ts
@@ -48,4 +48,70 @@ describe("Form", () => {
       .get(".no-task")
       .should("have.text", "All of your tasks are complete. Nicely done!");
   });
+
+  // it("takes a screenshot", () => {
+  //   // screenshot will be saved as
+  //   // cypress/screenshots/users.spec.js/my tests -- takes a screenshot.png
+  //   cy.screenshot();
+  // });
+
+  it("viewport-screenshot", () => {
+    // https://on.cypress.io/viewport
+
+    cy.viewport(320, 480);
+    cy.screenshot("viewport/my-todo-app-320x480");
+    cy.wait(200);
+
+    cy.viewport(2999, 2999);
+    cy.screenshot("viewport/my-todo-app-2999x2999");
+    cy.wait(200);
+
+    cy.viewport("macbook-15");
+    cy.screenshot("viewport/my-todo-app-macbook-15");
+    cy.wait(200);
+
+    cy.viewport("macbook-13");
+    cy.screenshot("viewport/my-todo-app-macbook-13");
+    cy.wait(200);
+
+    cy.viewport("macbook-11");
+    cy.screenshot("viewport/my-todo-app-macbook-11");
+    cy.wait(200);
+
+    cy.viewport("ipad-2");
+    cy.screenshot("viewport/my-todo-app-ipad-2");
+    cy.wait(200);
+
+    cy.viewport("ipad-mini");
+    cy.screenshot("viewport/my-todo-app-ipad-mini");
+    cy.wait(200);
+
+    cy.viewport("iphone-6+");
+    cy.screenshot("viewport/my-todo-app-ipad-6-plus");
+    cy.wait(200);
+
+    cy.viewport("iphone-6");
+    cy.screenshot("viewport/my-todo-app-ipad-6");
+    cy.wait(200);
+
+    cy.viewport("iphone-5");
+    cy.screenshot("viewport/my-todo-app-ipad-5");
+    cy.wait(200);
+
+    cy.viewport("iphone-4");
+    cy.screenshot("viewport/my-todo-app-ipad-4");
+    cy.wait(200);
+
+    cy.viewport("iphone-3");
+    cy.screenshot("viewport/my-todo-app-ipad-3");
+    cy.wait(200);
+
+    cy.viewport("ipad-2", "portrait");
+    cy.screenshot("viewport/my-todo-app-ipad-2-portrait");
+    cy.wait(200);
+
+    cy.viewport("iphone-4", "landscape");
+    cy.screenshot("viewport/my-todo-app-iphone-4-landscape");
+    cy.wait(200);
+  });
 });
